042ca71037c790dbfad513ff0f3ce411214a864f
Alexandre do Vale <vale.apv@gmail.com>
2018-05-20 15:39:44 -0300

    Estrutura do post com a função map

--- InstaluraMobile/index.js
+++ InstaluraMobile/index.js
@@ -1,12 +1,21 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions
+} from 'react-native';
+const width = Dimensions.get('screen').width;
 
 expoert default class InstaluraMobile extends Component {
 	render(){
 		<View style={styles.container}>
 			<Text style={styles.welcome}>
-				Olá Mundo
+				Alexandre
 			</Text>
+			<Image source={required('./resources/img/alura.png')} style={{width: width, height: width}} />
 		</View>
 	}
 }
