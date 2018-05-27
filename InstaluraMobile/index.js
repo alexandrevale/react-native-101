2226a6ac795f8127b075f1c8b627408222d5c3b0
Alexandre do Vale <vale.apv@gmail.com>
2018-05-27 20:27:31 -0300

    Criando a aplicação
    Capítulo 2

--- InstaluraMobile/index.js
+++ InstaluraMobile/index.js
@@ -1,32 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  Dimensions
-} from 'react-native';
-const width = Dimensions.get('screen').width;
-
-expoert default class InstaluraMobile extends Component {
-	render(){
-		<View style={styles.container}>
-			<Text style={styles.welcome}>
-				Alexandre
-			</Text>
-			<Image source={required('./resources/img/alura.png')} style={{width: width, height: width}} />
-		</View>
-	}
-}
-
-Const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#292929'
-	}
-});
 
 AppRegistry.registerComponent('InstaluraMobile', () => App);
