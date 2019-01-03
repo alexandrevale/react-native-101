02abc9c60a0361ad55ca2ed39eef1b3c89fe8948
Alexandre do Vale <vale.apv@gmail.com>
2019-01-03 14:27:16 -0200

    App Albums - Header Component
    Componente do header do app

---
+++ albums/src/components/header.js
@@ -0,0 +1,12 @@
+// Import
+import React from 'react';
+import ReactNative from 'react-native';
+import { Text } from 'react-native';
+
+// Component
+const Header = () => {
+	return <Text>Albums!</Text>;
+}
+
+// Make the component av. to other parts of the app
+export default Header;
