523a45346d2b372833022b4e6d3633d6809316aa
Alexandre do Vale <vale.apv@gmail.com>
2019-01-04 11:43:01 -0200

    App Albums - import do Axios no projeto e definição do state inicial

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import ReactNative from 'react-native';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+	state = {
+		albums: []
+	}
+
 	componentWillMount() {
-		// console.log('componentWillMount')
 		// debugger;
+		axios.get('http://rallycoding.herokuapp.com/api/music_albums');
+			.then( response => this.setState({ albums: response.data }));
 	}
 
 	render() {
+		console.log(this.state);
+		
 		return (
 			<View>
-				<Text></Text
+				<Text></Text>
 			</View>
 		);
 	}
