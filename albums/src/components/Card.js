55adf97f60a44ea7d1d4efe9ac52f0aeb029cd81
Alexandre do Vale <vale.apv@gmail.com>
2019-01-04 15:04:01 -0200

    App Albums - Style do AlbumDetail

---
+++ albums/src/components/Card.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactNative from 'react-native';
+import { View } from 'react-native';
+
+const Card = () => {
+	return(
+		<View style={styles.containerStyle}> </View>
+	)
+};
+
+const styles = {
+	containerStyle: {
+		borderWidth: 1,
+		borderRadius: 2,
+		borderColor: '#ddd',
+		borderBottomWidth: 0,
+		shadowColor: '#000',
+		shadowOffset: {
+			width: 0,
+			height: 2
+		},
+		shadowOpacity: 0.3,
+		shadowRadius: 2,
+		elevation: 1,
+		marginTop: 10
+		marginRight: 5,
+		marginBottom: 0,
+		marginLeft: 5,
+	}
+}
+
+export default Card;
