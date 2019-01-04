81af1e71051a56edbd5cb44c7a0ccb0d2afdde14
Alexandre do Vale <vale.apv@gmail.com>
2019-01-04 09:09:23 -0200

    App Albums - Generalização do título do componente Header e implementação do style

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,11 +1,35 @@
 // Import
 import React from 'react';
 import ReactNative from 'react-native';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Component
-const Header = () => {
-	return <Text>Albums!</Text>;
+const Header = (props) => {
+	const { textStyle, viewStyle } = styles;
+
+	return (
+		<View style={viewStyle}>
+			<Text style={textStyle}>{props.headerText}</Text>
+		</View>
+	);
+}
+
+const styles = {
+	viewStyle: {
+		backgroundColor: '#f8f8f8',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 60,
+		paddingTop: 15,
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.5,
+		elevation: 2,
+		position: 'relative'
+	},
+	textStyle {
+		fontSize: 20
+	}
 }
 
 // Make the component av. to other parts of the app
