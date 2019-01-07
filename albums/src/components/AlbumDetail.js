b19df611bbe7177deeb5e49d166ddec5d0966e90
Alexandre do Vale <vale.apv@gmail.com>
2019-01-06 23:41:17 -0200

    App Albums - Refact do component AlbumDetail inserindo imagem e detalhes do músico

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
 import ReactNative from 'react-native';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ data }) => {
+	const { title, artist, thumbnail_image } = data;
+	const { headerContentStyle, thumbnailStyle } = styles;
+
 	return (
 		<Card>
 			<CardSection>
-				<Text>{props.data.title}</Text>
+				<View>
+					<Image 
+						style={thumbnailStyle}
+						source={ { uri: thumbnail_image } } 
+					/>
+				</View>
+				<View style={headerContentStyle}">
+					<Text>{title}</Text>
+					<Text>{artist}</Text>
+				</View>
 			</CardSection>
 		</Card>
 	)
+}
+
+const styles = {
+	headerContentStyle: {
+		flexDirection: 'column',
+		justifyContent: 'space-around'
+	},
+	thumbnailStyle: {
+		width: 50,
+		height: 50
+	}
 }
 
 export default AlbumDetail;
