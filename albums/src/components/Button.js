32db05ebfe742aa92b586886e2e76777bc0eed55
Alexandre do Vale <vale.apv@gmail.com>
2019-01-07 19:54:18 -0200

    App Albums - Style do component Button

--- albums/src/components/Button.js
+++ albums/src/components/Button.js
@@ -3,11 +3,34 @@ import ReactNative from 'react-native';
 import { Text } from 'react-native';
 
 const Button = () => {
+	const { buttonStyle, textStyle } = styles;
+
 	return (
-		<TouchableOpacity>
-			<Text>Click me</Text>
+		<TouchableOpacity style={buttonStyle}>
+			<Text style={textStyle}>Click me</Text>
 		</TouchableOpacity>
 	)
 };
+
+const styles = {
+	buttonStyle: {
+		flex: 1,
+		alignSelf: 'stretch',
+		backgroundColor: '#fff',
+		borderRadius: 5,
+		borderWidth: 1,
+		borderColor: '#007aff',
+		marginLeft: 5,
+		marginRight: 5
+	},
+	textStyle: {
+		alignSelf: 'center',
+		color: '@007aff',
+		fontSize: 16,
+		fontWeight: '700',
+		paddingTop: 10,
+		paddingBottom: 10
+	}
+}
 
 export default Button;
