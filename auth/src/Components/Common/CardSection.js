095f439bab52028ea277250fbfe6ecc523f7db61
Alexandre do Vale <vale.apv@gmail.com>
2019-01-08 08:08:28 -0200

    App Auth - Arquivos iniciais e componentes básicos

---
+++ auth/src/Components/Common/CardSection.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactNative from 'react-native';
+import { View } from 'react-native';
+
+const CardSection = () => {
+	return (
+		<View style={styles.containerStyle}>
+			{props.children}
+		</View>
+	)
+}
+
+const styles = {
+	containerStyle: {
+		borderBottomWidth: 1,
+		padding: 5,
+		backgroundColor: '#fff',
+		justifyContent: 'flex-start',
+		flexDiretion: 'row',
+		borderColor: '#ddd',
+		position: 'relative'
+	}
+}
