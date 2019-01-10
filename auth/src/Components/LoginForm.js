ef51b3cc981b056e35a9931a14528d20a3405a63
Alexandre do Vale <vale.apv@gmail.com>
2019-01-09 23:20:39 -0200

    App Auth - Import e implementação do TextInput

--- auth/src/Components/LoginForm.js
+++ auth/src/Components/LoginForm.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
+	state = { text: '' };
+
 	render() {
 		return(
 			<Card>
 				<CardSection />
+					<TextInput 
+						value={this.state.text}
+						onChangeText={text => this.setState({ text })}
+						style={{ width: 100, height: 20 }}
+					/>
+				</CardSection>
 
 				<CardSection />
+					
+				</CardSection>
 
 				<CardSection>
 					<Button>
