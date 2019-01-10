5bfc5dd2ddff463358485e1cb95c9448bffedb05
Alexandre do Vale <vale.apv@gmail.com>
2019-01-09 23:05:05 -0200

    App Auth - Import e config Firebase

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from '.components/common';
 
-
 class App extends Component {
+	componentWillMount() {
+		// data from Firebase console
+		firebase.initializeApp({
+	    apiKey: '',
+	    authDomain: '',
+	    databaseURL: '',
+	    projectId: '',
+	    storageBucket: '',
+	    messagingSenderId: ''
+		});
+	}
+
 	render() {
 		return(
 			<View>
