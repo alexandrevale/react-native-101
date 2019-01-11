9dcdbf37c55026004cf6e8c769f2a7aeb5b85502
Alexandre do Vale <vale.apv@gmail.com>
2019-01-11 08:14:42 -0200

    App Auth - Eventos de autenticação

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from '.components/common';
+import { Header, Button, Spinner } from '.components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+	state = { loggedIn: null }; 
+
 	componentWillMount() {
 		// data from Firebase console
 		firebase.initializeApp({
@@ -15,13 +17,32 @@ class App extends Component {
 	    storageBucket: '',
 	    messagingSenderId: ''
 		});
+
+		firebase.auth().onAuthStateChanged((user) => {
+			if (user) {
+				this.setState({ loggedIn: true });
+			} else {
+				this.setState({ loggedIn: false });
+			}
+		});
+	}
+
+	renderContent() {
+		switch (this.state.loggedIn) {
+			case true:
+				return <Button>Logout</Button>;
+			case false:
+				return <LoginForm />;
+			default:
+				return <Spinner size="large" />;
+		}
 	}
 
 	render() {
 		return(
 			<View>
 				<Header headerText="Authentication" />
-				<LoginForm />
+				{this.renderContent()}
 			</View>
 		);
 	}
