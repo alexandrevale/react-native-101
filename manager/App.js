a65d5c7ef07e1620deb51c27adfcb422a0dbc0ac
Alexandre do Vale <vale.apv@gmail.com>
2019-01-16 20:31:38 -0200

    App Manager - Setup inicial
    Install e Import do redux, react-redux, firebase

---
+++ manager/App.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import { Text, View} from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+
+export default class App extends Component<Props> {
+  componentWillMount() {
+
+    // Initialize Firebase
+    var config = {
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: ""
+    };
+
+    firebase.initializeApp(config);
+  };
+  
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>Olá Mundo</Text>
+        </View>
+      </Provider>
+    );
+  }
+}
