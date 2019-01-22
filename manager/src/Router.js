df660e16231469aeeb184ff58ea5bf1b58fdd0b4
Alexandre do Vale <vale.apv@gmail.com>
2019-01-22 21:19:43 -0200

    App Manager - EmployeeList componenet
    Estrutura básica do componenet e a definição da initial scene em Routes

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
 	return (
 		<Router>
 			<Scene key="root">
-				<Scene key="login" component={LoginForm} title="Login"
+				<Scene key="login" component={LoginForm} title="Login" initial />
+				<Scene key="employeeList" component={EmployeeList} title="Funcionários" />
 			</Scene>
 		</Router>
 	);
---
+++ manager/src/components/EmployeeList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeList extends Component {
+	render() {
+		return (
+			<View>
+				<Text>Lista de funcionários</Text>
+				<Text>Lista de funcionários</Text>
+				<Text>Lista de funcionários</Text>
+			</View>
+		)
+	}
+}
+
+export default EmployeeList;
