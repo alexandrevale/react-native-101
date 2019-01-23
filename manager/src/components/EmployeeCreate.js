8ba1ef3c724789e69b8d64bf0e085afba69391e1
Alexandre do Vale <vale.apv@gmail.com>
2019-01-23 19:58:28 -0200

    App Manager - Atualizando o component EmployeeCreate

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 // import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
+
 
 class EmployeeCreate extends Component {
 	render() {
@@ -10,6 +13,8 @@ class EmployeeCreate extends Component {
 					<Input
 						label="Nome"
 						placeholder="Nome do funcionário"
+						value={this.props.name}
+						onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
 					/>
 				</CardSection>
 
@@ -17,6 +22,8 @@ class EmployeeCreate extends Component {
 					<Input
 						label="Telefone"
 						placeholder="(11) 98888-8888"
+						value={this.props.phone}
+						onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
 					/>
 				</CardSection>
 
@@ -35,4 +42,10 @@ class EmployeeCreate extends Component {
 	}
 }
 
-export default EmployeeCreate;
+const mapStateToProps = (state) => {
+	const { name, phone, shift } = state.EmployeeForm;
+
+	return { name, phone, shift }
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
