c5d3ae47f9596a020bf563f8f96d900931e62183
Alexandre do Vale <vale.apv@gmail.com>
2019-01-24 22:57:58 -0200

    App Manager - Fetch dos funcionários cadastrados

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -3,7 +3,8 @@ import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
 import {
 	EMPLOYEE_UPDATE,
-	EMPLOYEE_CREATE
+	EMPLOYEE_CREATE,
+	EMPLOYEES_FETCH_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -26,4 +27,15 @@ export const employeeCreate = ({ name, phone, shift }) => {
 				Actions.employeeList({ type: 'reset' })
 			});
 	};
+};
+
+export const employeesFetch = () => {
+	const { currentUser } = firebase.auth();
+
+	return (dispatch) => {
+		firebase.database().ref(`/users/${currentUser.uid}/employees`)
+			.on('value', snapshot => {
+				dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
+			});
+	}
 };
--- manager/src/components/EmployeeList.js
+++ manager/src/components/EmployeeList.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { ListView, View, Text } from 'react-native';
+import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
+	componentWillMount() {
+		this.props.employeesFetch();
+
+		this.createDataSource(this.props);
+	}
+
+	componentWillReceiveProps(nextProps) {
+		/* 
+			nextProps é o próxixo conjunto de props 
+			que o component renderizará	
+
+			this.props continuará sendo o antigo o
+			conjunto de props
+		*/
+
+		this.createDataSource(nextProps);
+
+	}
+
+	createDataSource({ employees }) {
+		const ds = new ListView.DataSource({
+			rowHasChanged: (r1, r2) => r1 !== r2
+		});
+
+		this.dataSource = ds.cloneWithRows(employees);
+	}
+
 	render() {
 		return (
 			<View>
@@ -13,4 +42,4 @@ class EmployeeList extends Component {
 	}
 }
 
-export default EmployeeList;
+export default connect(null, { employeesFetch })EmployeeList;
