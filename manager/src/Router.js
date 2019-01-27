72fad29dd935e205248fe48083da864c47f6ed82
Alexandre do Vale <vale.apv@gmail.com>
2019-01-27 14:16:52 -0200

    App Manager - EmployeeEdit component

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -3,6 +3,7 @@ import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
 import EmployeeCreate from '.components/EmployeeCreate';
+import EmployeeEdit from './components/EmployeeEdit';
 
 const RouterComponent = () => {
 	return (
@@ -29,6 +30,12 @@ const RouterComponent = () => {
 						key="employeeCreate" 
 						title="Novo Funcionário" 
 						component={EmployeeCreate} 
+					/>
+
+					<Scene
+						key="employeeEdit"
+						component={EmployeeEdit}
+						title="Editar Funcionário"
 					/>
 
 				</Scene>
