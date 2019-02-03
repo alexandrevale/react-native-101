d9ef561ff4c553bdd5e0a171dd7309cf76cdb1ef
Alexandre do Vale <vale.apv@gmail.com>
2019-02-03 18:44:19 -0200

    App Rocket Uber - state de controle do input
    Utilizado para fechar a pesquisa onBlur

--- rocketUber/src/components/Search/index.js
+++ rocketUber/src/components/Search/index.js
@@ -3,6 +3,10 @@ import { Platform } from 'react-native';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 export default class Search extends Component {
+  state = {
+  	searchFocused: false,
+  }
+
   render() {
   	const { onLocationSelected } = this.props;
 
@@ -15,9 +19,16 @@ export default class Search extends Component {
     		language: 'pt'
     	}}
     	textInputProps={{
+    		onFocus: () => { 
+    			this.setState({ searchFocused: true }) 
+    		},
+    		onBlur: () => { 
+    			this.setState({ searchFocused: false }) 
+    		},
     		autoCapitalize: "none",
     		autoCorrect: false
     	}}
+    	listViewDisplayed={searchFocused}
     	fetchDetails
     	enablePoweredByContainer={false}
     	styles={{
